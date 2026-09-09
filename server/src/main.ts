654fcf584f4adf35a6c864868f38566609b90d3b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-09-09 21:10:22 +0500

    update main

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+
+app.all("/api/auth/{*splat}", toNodeHandler(auth));
 
 const PORT = Number(process.env.PORT);
 
