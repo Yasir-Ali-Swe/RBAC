31f36610172105f5d9b164928a7fba2f77c27c39
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-09-12 21:27:19 +0500

    update auth

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -6,6 +6,12 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    trustedOrigins: [
+        process.env.FRONTEND_URL || "http://localhost:3000",
+        "http://localhost:3000",
+        "http://localhost:5000",
+        "http://localhost:5001",
+    ],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
